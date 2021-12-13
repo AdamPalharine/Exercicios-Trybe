d3fbc33da9540552e369e774498e72cfda98c35a
Adam <anypalharine@gmail.com>
2021-12-13 18:34:37 -0300

    Elementos e 1 ao 6 feitos

--- dia-2-javascript-trabalhando-com-elementos/para-fixar/script-2.js
+++ dia-2-javascript-trabalhando-com-elementos/para-fixar/script-2.js
@@ -1,30 +1,23 @@
-console.log(document.getElementById('elementoOndeVoceEsta'))
+let ondeEsta = document.getElementById('elementoOndeVoceEsta');
 
-console.log(document.getElementById('elementoOndeVoceEsta').parentElement)
+let elementoPai = document.getElementById('elementoOndeVoceEsta').parentElement;
+elementoPai.style.color = 'red';
 
-/*  // 1. Acesse o elemento elementoOndeVoceEsta.
- const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+let elementoFilho = elementoOndeVoceEsta.firstElementChild;
+elementoFilho.innerText = 'Sei lá';
 
- // 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
- const pai = elementoOndeVoceEsta.parentElement;
- pai.style.color = 'red';
+let primeiroFilho = pai.firstElementChild;
+console.log(primeiroFilho);
 
- // 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
- // Você se lembra dos vídeos da aula anterior, como fazer isso?
- const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
- primeiroFilhoDoFilho.innerText = 'primeiroFilhoDoFilho';
+let primeiroFilhoFilho = elementoOndeVoceEsta.previousElementSibling;
+console.log(primeiroFilhoFilho);
 
- // 4. Acesse o primeiroFilho a partir de pai.
- const primeiroFilho = pai.firstElementChild;
+let primeiroNo = elementoOndeVoceEsta.nextSibling;
 
- // 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
- const primeiroFilho2 = elementoOndeVoceEsta.previousElementSibling;
- 
- // 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
- const textElement = elementoOndeVoceEsta.nextSibling;
+/* 
 
- // 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
- const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
+// 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
 
- // 8. Agora acesse o terceiroFilho a partir de pai.
- const terceiroFilho2 = pai.lastElementChild.previousElementSibling; */
+// 8. Agora acesse o terceiroFilho a partir de pai.
+const terceiroFilho2 = pai.lastElementChild.previousElementSibling; */
