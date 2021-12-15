89a7c256678f2d015233a76a5852590138a49445
Adam <anypalharine@gmail.com>
2021-12-14 21:44:20 -0300

    Resolvendo funçoes 2 e 3

--- dia-3-javascript-eventos/para-fixar.js
+++ dia-3-javascript-eventos/para-fixar.js
@@ -12,17 +12,24 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- 
-/* firstLi.addEventListener('click', clicar);
-
-function clicar() {	
-    firstLi.innerText = "Clicou!";
-    firstLi.style.background = "blue";
-    }
- */
+function handleChangeTech(event) {
+    const techElement = document.querySelector('.tech');
+    techElement.classList.remove('tech');
+    event.target.classList.add('tech');
+    input.value = '';
+  }
+  
+  firstLi.addEventListener('click', handleChangeTech);
+  secondLi.addEventListener('click', handleChangeTech);
+  thirdLi.addEventListener('click', handleChangeTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
+input.addEventListener('input', function(event) {
+  const techElement = document.querySelector('.tech');
+  techElement.innerText = event.target.value;
+});
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
