ab32c91869333712aa1aad523992dffce2186dea
Adam <anypalharine@gmail.com>
2021-12-16 15:38:00 -0300

    Implementando texto nas sexta-feira do calendario

--- dia-3-javascript-eventos/calendario.js
+++ dia-3-javascript-eventos/calendario.js
@@ -20,11 +20,11 @@ function createDaysOfTheWeek() {
   let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
   function criandoDiasDoMes() {
-    let getDaysList = document.querySelector('#days');
+    let getDaysList = document.querySelector('#days'); //selecionar o elemente que tem o id Days
     
     for (let i = 0; i < dezDaysList.length; i += 1){
-      let day = dezDaysList[i];
-      let dayItem = document.createElement('li');
+      let day = dezDaysList[i]; //em cada interação armazena uma variavel - o dia
+      let dayItem = document.createElement('li'); //criando o list item que vai representar o dia
 
       if (day === 24 | day === 31) {
         dayItem.className = 'day holiday';
@@ -50,14 +50,70 @@ function createDaysOfTheWeek() {
 
 // criando botão - Exercicio 2
 
-function createHolidayButton(buttonName) {
-  let buttonContainer = document.querySelector('.buttons-container');
-  let newButton = document.createElement('button');
-  let newButtonID = 'btn-holiday';
+function botaoFeriados(buttonName) {
+  let buttonContainer = document.querySelector('.buttons-container'); // onde vou adicionar o botão
+  let newButton = document.createElement('button'); // criando o botão
+  let newButtonID = 'btn-holiday'; // criando a ID do botão
 
   newButton.innerHTML = buttonName;
-  newButton.id = newButtonID;
-  buttonContainer.appendChild(newButton);
+  newButton.id = newButtonID; // adicionando o ID ao botão
+  buttonContainer.appendChild(newButton); // declarando ele como filho da função
 };
 
-createHolidayButton('Feriados');
+botaoFeriados('Feriados');
+
+// implementando cores ao botão - Exercicio 3
+
+function displayHolidays() {
+  let getHolidayButton = document.querySelector('#btn-holiday');
+  let getHolidays = document.querySelectorAll('.holiday')
+  let backgroundColor = 'rgb(238,238,238)';
+  let setNewColor = 'white';
+
+  getHolidayButton.addEventListener('click', function() {
+    for (let index = 0; index < getHolidays.length; index += 1) {
+      if (getHolidays[index].style.backgroundColor === setNewColor) {
+        getHolidays[index].style.backgroundColor = backgroundColor;
+      } else {
+        getHolidays[index].style.backgroundColor = setNewColor;
+      }
+    }
+  })
+};
+
+displayHolidays();
+
+// Implementando o botão - Exercicio 4
+
+function botaoFriday(buttonFriday) {
+  let buttonContainer = document.querySelector('.buttons-container'); // onde vou adicionar o botão
+  let newButton = document.createElement('button'); // criando o botão
+  let newButtonID = 'btn-friday'; // criando a ID do botão
+
+  newButton.innerHTML = buttonFriday;
+  newButton.id = newButtonID; // adicionando o ID ao botão
+  buttonContainer.appendChild(newButton); // declarando ele como filho da função
+};
+
+botaoFriday('Sexta-Feira');
+
+// Exercicio 5 - Evento click ao botão Sexta-feira
+
+function displayFridays(sextaArray) {
+  let sextaDoMes = document.querySelector('#btn-friday');
+  let sextaFeira = document.getElementsByClassName('friday')
+  let novoTexto = "Sextou"; 
+
+  sextaDoMes.addEventListener('click', function() {
+    for (let i = 0; i < sextaFeira.length; i += 1) {
+      if (sextaFeira[i].innerHTML !== novoTexto) {
+        sextaFeira[i].innerHTML = novoTexto;
+      } else {
+        sextaFeira[i].innerHTML = sextaArray[i];
+      }
+    }
+  })
+};
+
+let diaSexta = [ 4, 11, 18, 25];
+displayFridays(diaSexta);
