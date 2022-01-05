de159019c19d5ca3ec35100169a50bc0f472592c
Adam <anypalharine@gmail.com>
2022-01-04 21:14:40 -0300

    fazendo o botao para apagar

--- bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,1 +1,13 @@
 const listaEstado = document.getElementById('lista');
+const limparLista = document.getElementById('limpar');
+const pai = document.getElementsByClassName('bloco');
+const filhos = pai.children;
+
+limparLista.addEventListener('click', apagar)
+
+function apagar(){
+    const apagando = document.getElementsByTagName('input')
+    for (let i = pai.length; i > 0 ; i -= 1) {
+        pai.removeChild(apagando[0]);
+    }    
+}
