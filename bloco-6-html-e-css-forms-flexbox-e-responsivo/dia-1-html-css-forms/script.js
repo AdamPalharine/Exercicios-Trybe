9e15f0b9780bea05bed449e7eb84b105d438a963
Adam <anypalharine@gmail.com>
2022-01-05 13:48:55 -0300

    estilo adicionado e botao dos Estados funcionando

--- bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,7 +1,4 @@
 const listaEstado = document.getElementById('lista');
-const limparLista = document.getElementById('limpar');
-const divs = document.getElementById("form");
-const filhos = divs.children;
 
 function createStateOptions() {
   const stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
@@ -12,15 +9,6 @@ function createStateOptions() {
     option.value = stateOptions[i];
     listaEstado.appendChild(option);
   }
-}
-
-limparLista.addEventListener('click', apagar)
-
-function apagar(){
-    const apagando = document.getElementsByClassName('form')
-    for (let i = filhos.length; i > 0 ; i -= 1) {
-        divs.removeChild(apagando[0]);
-    }    
 }
 
 createStateOptions();
