eb78bdc48853a9e1614bd68468f2f6554791760f
Adam <anypalharine@gmail.com>
2022-01-04 21:32:47 -0300

    Tentando o botao e criar os Estados

--- bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,13 +1,26 @@
 const listaEstado = document.getElementById('lista');
 const limparLista = document.getElementById('limpar');
-const pai = document.getElementsByClassName('bloco');
-const filhos = pai.children;
+const divs = document.getElementById("form");
+const filhos = divs.children;
+
+function createStateOptions() {
+  const stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+
+  for (let i = 0; i < stateOptions.length; i += 1) {
+    const option = document.createElement('option');
+    option.innerText = stateOptions[i];
+    option.value = stateOptions[i];
+    listaEstado.appendChild(option);
+  }
+}
 
 limparLista.addEventListener('click', apagar)
 
 function apagar(){
-    const apagando = document.getElementsByTagName('input')
-    for (let i = pai.length; i > 0 ; i -= 1) {
-        pai.removeChild(apagando[0]);
+    const apagando = document.getElementsByClassName('form')
+    for (let i = filhos.length; i > 0 ; i -= 1) {
+        divs.removeChild(apagando[0]);
     }    
 }
+
+createStateOptions();
