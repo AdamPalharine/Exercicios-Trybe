54ae1f3ec8c19c329e8ee8146347be029cd92756
Adam <anypalharine@gmail.com>
2022-01-18 18:25:32 -0300

    adicionando o e-mail ao lado do nome

--- bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script-1.js
+++ bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script-1.js
@@ -1,23 +1,17 @@
 const newEmployees = (callback) => {
-    const employees = {
-      id1: callback("Pedro Guerra"), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: callback("Luiza Drumond"), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: callback("Carla Paiva"), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-    };
-    return employees;
+  const employees = {
+    id1: callback("Pedro Guerra"), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback("Luiza Drumond"), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback("Carla Paiva"), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   };
+  return employees;
+};
 
-  const criarEmail = (nome) => {
-      return {
-          nome: nome,
-          email: 'undefine@trybe.com',
-      };
+const criarEmail = (nome) => {
+  const email = nome.toLowerCase().split(' ').join('_'); //  > nome.emMinusculo.removeEspaço.substituiPor'_'
+  return {
+    nome: nome, email: `${email}@trybe.com`,
   };
+};
 
-  console.log(newEmployees(criarEmail))
-/* const employees = {
-  id1: 'pedro_guerra@trybe.com',
-  id2: 'luiza_drumond@trybe.com',
-  id3: 'carla_paiva@trybe.com',
-}
- */
+console.log(newEmployees(criarEmail)) // > console.log(newEmployees(callback))
