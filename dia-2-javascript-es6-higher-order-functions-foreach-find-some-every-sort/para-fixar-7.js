610185f9785a607a406b062354ba2a2d663c0a35
Adam <anypalharine@gmail.com>
2022-01-19 19:22:31 -0300

    Fazendo os exercicios Para Fixar

---
+++ dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/para-fixar-7.js
@@ -0,0 +1,23 @@
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const people2 = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+  
+  // Ordem crescente
+  people.sort((a, b) => a.age - b.age);
+  // Ordem decrescente
+  people2.sort((a, b) => b.age - a.age);
+
+  console.log(people);
+  console.log(people2);
