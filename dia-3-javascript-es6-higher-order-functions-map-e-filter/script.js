8aca1b9fa8c33f3ee0fee4d5180e09f8a7d341d9
Adam <anypalharine@gmail.com>
2022-01-20 21:14:48 -0300

    Resolvendo Exercicio 2

--- dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
+++ dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
@@ -69,40 +69,23 @@ function formatedBookNames() {
 };
 
 console.log(formatedBookNames());
+console.log(' ');
 
-/* // 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
-const expectedResult = [
-  {
-    age: 31,
-    author: 'Isaac Asimov',
-  },
-  {
-    age: 38,
-    author: 'H. P. Lovecraft',
-  },
-  {
-    age: 39,
-    author: 'Stephen King',
-  },
-  {
-    age: 43,
-    author: 'George R. R. Martin',
-  },
-  {
-    age: 45,
-    author: 'Frank Herbert',
-  },
-  {
-    age: 62,
-    author: 'J. R. R. Tolkien',
-  },
-];
-
+// 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
 function nameAndAge() {
-  // escreva seu código aqui
+  return books.map((book) => (
+    { 
+      author: book.author.name, 
+      age: book.releaseYear - book.author.birthYear ,
+    }
+  ))
+  .sort((a, b) => a.age - b.age);
 }
 
-// 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+console.log(nameAndAge());
+console.log(' ');
+
+/* // 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
 const expectedResult = [
   { 
     id: 1,
