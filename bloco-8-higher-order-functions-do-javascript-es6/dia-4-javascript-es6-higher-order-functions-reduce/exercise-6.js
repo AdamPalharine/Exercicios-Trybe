9afb3857cd7835423267e4619b28ae73d8672649
Adam <anypalharine@gmail.com>
2022-01-25 01:08:56 -0300

    Resolvendo exercicio 6

--- bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise-6.js
+++ bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise-6.js
@@ -4,11 +4,21 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
-  // escreva seu código aqui
-}
+  return students.map((student, index) => ({
+    name: student,
+    nota: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
+  }))
+};
 
-/* const expected = [
-  { name: 'Pedro Henrique', average: 7.8 },
-  { name: 'Miguel', average: 9.2 },
-  { name: 'Maria Clara', average: 8.8 },
-]; */
+console.log(studentAverage());
+
+/* function studentAverage() {
+  const nomeMaisNota = students.map((student, index) => ({
+    name: student,
+    nota: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
+  }));
+  return nomeMaisNota;
+};
+
+console.log(studentAverage());
+ */
