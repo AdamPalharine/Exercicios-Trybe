44856bc24b60327d51e7e791ece64e1ed67749ed
Adam <anypalharine@gmail.com>
2022-02-04 17:02:13 -0300

    problemas com o selected

--- dia-4-javascript-web-storage/script.js
+++ dia-4-javascript-web-storage/script.js
@@ -59,8 +59,33 @@ function createFontTipeOptions() {
   }
 }
 
+// selected em cada selected
+
+function criandoSelected() {
+  coresFundo.children[0].classList.add('selected');
+  coresTexto.children[0].classList.add('selected');
+  tamanhoFonte.children[0].classList.add('selected');
+  linhaEspaco.children[0].classList.add('selected');
+  tiposFonte.children[0].classList.add('selected');
+}
+
+// movendo selected (funciona, mas movendo pro lugar errado)
+
+function movendoSelected(event) {
+  let techElement = document.getElementsByClassName('selected');
+  const evento = event.target;
+  techElement[0].classList.remove('selected');
+  evento.classList.add('selected');
+}
+
 createColorsOptions();
 createFontOptions();
 createFontSizeOptions();
 createAlignOptions();
 createFontTipeOptions();
+criandoSelected();
+coresFundo.addEventListener('change', movendoSelected);
+coresTexto.addEventListener('change', movendoSelected);
+tamanhoFonte.addEventListener('change', movendoSelected);
+linhaEspaco.addEventListener('change', movendoSelected);
+tiposFonte.addEventListener('change', movendoSelected);
