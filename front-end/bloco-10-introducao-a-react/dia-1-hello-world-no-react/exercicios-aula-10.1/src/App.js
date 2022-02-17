7a0b739b56b8a1fad017318a61e8729bec28aa76
Adam <anypalharine@gmail.com>
2022-02-17 02:49:54 -0300

    Exercicio 1 'feito' - problemas para rodar

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-aula-10.1/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-aula-10.1/src/App.js
@@ -1,6 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+// npx create-react-app é pesado demais para ficar criando vários, apenas comenta o exercicio 
+// Exercicio 1 
+/* 
+const Task = (value) => {
+  return (
+    <li key={value}> {value} </li>
+  );
+}
+
+const myList = ['Acordar Cedo', 'Escovar os Dentes', 'Tomar Café', 'Estudar', 'Almoçar', 'Estudar', 'Jantar'];
+
+class App extends React.Component {
+  render() {
+    return (
+      <ul>{ myList.map(myList => Task(myList)) }</ul>
+    );
+  }
+} 
+*/
+
 function App() {
   return (
     <div className="App">
