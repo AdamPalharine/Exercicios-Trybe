fe8045c7d17c3f175b0033709161b67fb9aeaf52
Adam <anypalharine@gmail.com>
2022-02-17 02:06:47 -0300

    testando ocreate-react-app e vendo q pode dar ruim

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/para-fixar-1.js
@@ -0,0 +1,7 @@
+// Para fixar, crie um elemento JSX que recebe o valor "Hello, JSX" de uma constante chamada textJSX , e o incorpore em uma tag h1 .
+
+// com JSX
+const textJSX = <h1>Hello, JSX</h1>;
+
+//sem JSX
+const textJSX = React.createElement("h1", null, "Hello, JSX");
