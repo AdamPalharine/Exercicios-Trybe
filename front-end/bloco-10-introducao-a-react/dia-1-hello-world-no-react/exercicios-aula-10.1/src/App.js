bb59f4e8d8b6da6091b5c8d9108434c00904271f
Adam <anypalharine@gmail.com>
2022-02-17 03:07:18 -0300

    Base do React

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-aula-10.1/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-aula-10.1/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 // npx create-react-app é pesado demais para ficar criando vários, apenas comenta o exercicio 
-// Exercicio 1 
+// Exercicio 1 que na verdade é o 2
 /* 
 const Task = (value) => {
   return (
@@ -21,6 +21,30 @@ class App extends React.Component {
 } 
 */
 
+//Exercicio 2 que na verdade é o 3
+// Importando tudo
+
+import React from 'react';
+
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+import './App.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    )
+  }
+} 
+
+// não entendi se precisa dessa parte é necessária...
 function App() {
   return (
     <div className="App">
