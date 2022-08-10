108515f5233ad40583cb44228e11dce530d0e390
Adam <anypalharine@gmail.com>
2022-08-10 04:30:53 -0300

    exemplo 3 concluido

--- Back-End/bloco-22-Web-com-Node/dia-3/exemplos/exe-3/tests/calculaArea.js
+++ Back-End/bloco-22-Web-com-Node/dia-3/exemplos/exe-3/tests/calculaArea.js
@@ -1,7 +1,26 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const { calculaArea } = require('../calculaArea');
+const readline = require('readline-sync');
+
 describe('Calcula a área de um quadrado', () => {
   describe('Quando a pessoa usuaria passa o valor 10', () => {
-    it ('é igual a 100', () => {
 
+    before(() => {
+      sinon.stub(readline, 'questionInt').returns(10);
+    });
+
+    after(() => {
+      readline.questionInt.restore();
+    });
+
+
+    it ('é igual a 100', () => {
+      const resposta = calculaArea();
+      expect(resposta).to.be.equal(100)
     });
   });
 });
+
+// nesse caso precisamos criar um duble(sinon) para simular a entrada de um numero novalor da equação, pq no teste não deveria ter entrada de valores
