53ac00baa0aee121a3e418d90d8e115c94fb4ca0
Adam <anypalharine@gmail.com>
2022-08-10 03:10:37 -0300

    criando a base dos testes

---
+++ Back-End/bloco-22-Web-com-Node/dia-3/exemplos/exe-1/tests/lerArquivo.js
@@ -0,0 +1,15 @@
+describe('Chama a função "ler arquivo"', () => {
+  describe('Quando o arquivo é lido com sucesso', () => {
+    it('é igual ao conteudo do arquivo', () =>{
+
+    });
+  });
+});
+
+describe('Quando a leitura do arquivo falha', () => {
+  describe('a resposta', () => {
+    it('é igual null', () =>{
+
+    });
+  });
+});
