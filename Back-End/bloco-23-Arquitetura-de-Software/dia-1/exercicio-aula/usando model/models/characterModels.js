f000436d5a30f289820b33d3fcb36cadaece793e
Adam <anypalharine@gmail.com>
2022-08-12 06:16:12 -0300

    tranformando código de 114 em um de 10 linhas

---
+++ Back-End/bloco-23-Arquitetura-de-Software/dia-1/exercicio-aula/usando model/models/characterModels.js
@@ -0,0 +1,51 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'nome_do_banco',
+  password: 'password'
+});
+
+const getAll = async () => {
+  const [characters] = await connection.execute('SELECT * FROM characters');
+  return characters;
+};
+
+const create = async (name, cartoon) => {
+  const [rows] = await connection.execute(
+    'INSERT INTO nome_do_banco.nome_da_tabela (name, cartoon) VALUES (?, ?);',
+    [name, cartoon]
+    );
+  return rows;
+};
+
+const getById = async (id) => {
+  const [rows] = await connection.execute(
+    'SELECT * FROM characters WHERE id = ?;',
+    [id]);
+  return rows;
+};
+
+const edit = async (name, cartoon, id) => {
+  const [characters] = await connection.execute(
+    'UPDATE onde_quero_editar SET name = ?, cartoon = ? WHERE id = ?;',
+    [name, cartoon, id]
+    );
+  return characters;
+};
+
+const removeInfo = async (id) => {
+  const [characters] = await connection.execute(
+    'DELETE FROM characters WHERE id=?;' [id]
+    );
+  return characters;
+};
+
+module.exports = {
+  getAll,
+  create,
+  getById,
+  edit,
+  removeInfo
+}
