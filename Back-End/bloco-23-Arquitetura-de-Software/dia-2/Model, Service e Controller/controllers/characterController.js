9832815d8d715b4876e3551c05a92af2e7292653
Adam <anypalharine@gmail.com>
2022-08-15 01:52:33 -0300

    terminando a pasta de Controller

--- Back-End/bloco-23-Arquitetura-de-Software/dia-2/Model, Service e Controller/controllers/characterController.js
+++ Back-End/bloco-23-Arquitetura-de-Software/dia-2/Model, Service e Controller/controllers/characterController.js
@@ -2,13 +2,13 @@ const characterModel = require('../models/characterModels');
 
 const ERROR_MESSAGE = 'Server error';
 
-const getAll = async (req, res) => {
+const getAll = async (_req, res) => {
   try {
     const characters = await characterModel.getAll(); 
     return res.status(200).json(characters);
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ message: ERROR_MESSAGE});
+    return res.status(500).json({ message: ERROR_MESSAGE });
   }
 };
 
@@ -25,11 +25,11 @@ const create = async (req, res) => {
     return res.status(200).json(rows);
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ message: ERROR_MESSAGE});
+    return res.status(500).json({ message: ERROR_MESSAGE });
   }
 };
 
-const getAllById = async (req, res) => {
+const getById = async (req, res) => {
   const { id } = req.params; 
   try {
     const rows = await characterModel.getById(id);
@@ -37,12 +37,39 @@ const getAllById = async (req, res) => {
       return res.status(200).json(rows);
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ message: ERROR_MESSAGE});
+    return res.status(500).json({ message: ERROR_MESSAGE });
+  }
+};
+
+const edit = async (req, res) => {
+  const { id } = req.params;
+  const { name, cartoon } = req.body
+  try {
+    const result = await characterModel.edit({ id, name, cartoon });
+    console.log(result);
+    return res.status(200).json({ id: Number(id), name, cartoon});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: ERROR_MESSAGE });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await characterModel.removeInfo(id);
+    console.log(result);
+    return res.status(200).json({ message: 'Informação Deletada'});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: ERROR_MESSAGE });
   }
 };
 
 module.exports = {
   getAll,
   create,
-  getAllById
+  getById,
+  edit,
+  remove
 };
--- Back-End/bloco-23-Arquitetura-de-Software/dia-2/Model, Service e Controller/routes/characterRouter.js
+++ Back-End/bloco-23-Arquitetura-de-Software/dia-2/Model, Service e Controller/routes/characterRouter.js
@@ -1,41 +1,18 @@
 const express = require('express');
-const characterModel = require('../models/characterModels');
-const characterController = require('../controllers/characterController')
 
-const ERROR_MESSAGE = 'Server error';
+const characterController = require('../controllers/characterController')
 
 const characterRouter = express.Router();
 
 characterRouter.get('/', characterController.getAll);  
+
 characterRouter.post('/', characterController.create);
 // ou characterRouter.post('/', (req, res => characterController.create(req, res)));
-characterRouter.get('/:id', characterController.getAllById);
 
-// PARA FAZER EDIÇÕES
-characterRouter.put('/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name, cartoon } = req.body
-  try {
-    const result = await characterModel.edit({ id, name, cartoon });
-    console.log(result);
-    return res.status(200).json({ id: Number(id), name, cartoon});
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: ERROR_MESSAGE});
-  }
-});
+characterRouter.get('/:id', characterController.getById);
 
-// PARA DELETAR
-characterRouter.delete('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const result = await characterModel.removeInfo(id);
-    console.log(result);
-    return res.status(200).json({ message: 'Informação Deletada'});
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: ERROR_MESSAGE});
-  }
-});
+characterRouter.put('/:id', characterController.edit);
+
+characterRouter.delete('/:id', characterController.remove);
 
 module.exports = characterRouter;
