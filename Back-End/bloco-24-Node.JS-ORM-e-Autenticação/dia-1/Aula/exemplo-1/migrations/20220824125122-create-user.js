5fadff8c8bac47a54b03aecd1d2b2d96d82aa1a6
Adam <anypalharine@gmail.com>
2022-08-24 11:20:11 -0300

    creado as seeds

--- Back-End/bloco-24-Node.JS-ORM-e-Autenticação/dia-1/Aula/exemplo-1/migrations/20220824125122-create-user.js
+++ Back-End/bloco-24-Node.JS-ORM-e-Autenticação/dia-1/Aula/exemplo-1/migrations/20220824125122-create-user.js
@@ -1,7 +1,11 @@
 'use strict';
 module.exports = {
+  /**
+   * @param {import('sequelize').queryInterface} queryInterface 
+   * @param {import('sequelize').Sequelize} Sequelize 
+   */
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -11,17 +15,35 @@ module.exports = {
       email: {
         type: Sequelize.STRING
       },
+      passwordHash:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'password_hash'
+        // para não trocar a nomenclatura que estiver na tabela
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        // field não necessario pq pegará como padrão
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,   
+      },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        // prestar mais atenção na hora de nomear pra não dar erro depois na hora de exportar as seeds
+        field: 'create_at'
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        field: 'updated_at',
       }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   }
 };
